4143a9e4630f8bfb29fccbe74f93fce08ca946c9
Alexandr <canek.seva@gmail.com>
2022-10-17 18:18:38 +0300

    add z8

---
+++ Zadacha8_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A ");
+int A = Convert.ToInt32(Console.ReadLine());
+int current = 0;
+while (current <= A)
+{
+    Console.WriteLine(current + " ");
+current = current + 2;
+}
