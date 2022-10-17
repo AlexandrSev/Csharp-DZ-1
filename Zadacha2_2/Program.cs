00938eadebd26969bfe2d5477a3deb4d184d0ad9
Alexandr <canek.seva@gmail.com>
2022-10-17 17:36:27 +0300

    End dz 2

---
+++ Zadacha2_2/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 2;
+int b = 10;
+
+if (a < b) Console.WriteLine($"{b} > {a}");
+if (a > b) Console.WriteLine($"{b} < {a}");
+if (a == b) Console.WriteLine($"{b} = {a}");
---
+++ Zadacha2_3/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = -9;
+int b = -3;
+
+if (a < b) Console.WriteLine($"{b} > {a}");
+if (a > b) Console.WriteLine($"{b} < {a}");
+if (a == b) Console.WriteLine($"{b} = {a}");
