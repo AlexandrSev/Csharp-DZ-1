919a199c74d247a764edb6fd93c6c4e7d7708e2a
Alexandr <canek.seva@gmail.com>
2022-10-17 18:24:50 +0300

    add end dz

---
+++ Zadacha6_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 2;
+int result = A % B;
+if (result > 0) Console.WriteLine("Нечетное");
+if (result == 0) Console.WriteLine("Четное");
+
+
